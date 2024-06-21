e8c55aeb77954cf233d7de7d1533b57534dd538e
Taejin Kim <rlaxowls1316@naver.com>
2024-06-21 15:17:57 +0900

    Flexbox 알아보기

--- App.tsx
+++ App.tsx
@@ -6,25 +6,42 @@
  */
 
 import React from 'react';
-import {Button, SafeAreaView, StyleSheet, Text, TextInput} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
 
 function App(): JSX.Element {
   return (
     // view는 웹에서의 div와 유사
     <SafeAreaView style={styles.container}>
-      <Text>Hello react native</Text>
-      <Button title="버튼" onPress={() => console.log('클릭됨')} />
-      <TextInput />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} />
+        <Text>Hello react native</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} />
+        <Text>Hello react native</Text>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: 'dodgerblue',
+  },
+  input: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: 'black',
+    height: 100,
+    width: 100,
+  },
+  inputContainer: {
+    // flex: 1,
     backgroundColor: 'red',
-    margin: '10%',
-    marginHorizontal: 10,
-    marginVertical: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
 });
 
