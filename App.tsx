fb877fd91ea3dd32bd3c7932f646fe7d5242e3f8
Taejin Kim <rlaxowls1316@naver.com>
2024-06-21 15:23:14 +0900

    이벤트 처리하기

--- App.tsx
+++ App.tsx
@@ -5,20 +5,27 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
 
 function App(): JSX.Element {
+  const [name, setName] = useState('');
+
+  const handleChangeInput = (text: string) => {
+    console.log(text);
+    setName(text);
+  };
+
   return (
     // view는 웹에서의 div와 유사
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} />
-        <Text>Hello react native</Text>
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input} />
-        <Text>Hello react native</Text>
+        <Text>이름</Text>
+        <TextInput
+          style={styles.input}
+          value={name}
+          onChangeText={handleChangeInput}
+        />
       </View>
     </SafeAreaView>
   );
@@ -27,18 +34,16 @@ function App(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
   },
   input: {
     flex: 1,
     borderWidth: 2,
     borderColor: 'black',
-    height: 100,
+    height: 50,
     width: 100,
   },
   inputContainer: {
-    // flex: 1,
-    backgroundColor: 'red',
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
